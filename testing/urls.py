a70077d5992de29e47603a241ec1070981cf4d14
Omnisslayer01 <abduldiyandeshmukh2005@gmail.com>
2026-05-16 22:15:44 +0530

    removed /admin routing from testing app - it shouldnt be there as it is already present in the project BuildCheck_AI url

--- testing/urls.py
+++ testing/urls.py
@@ -3,7 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('api/update-status/', views.update_bug_status, name='update_bug_status'),
 ]
 
