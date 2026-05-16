574578c532193fd784cc1e78664380bb46b84a3b
Omnisslayer01 <abduldiyandeshmukh2005@gmail.com>
2026-05-16 16:56:58 +0530

    Initialised django app for the testing part of BuildTest-AI

---
+++ BuildCheck_AI/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for BuildCheck_AI project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/6.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BuildCheck_AI.settings')
+
+application = get_asgi_application()
